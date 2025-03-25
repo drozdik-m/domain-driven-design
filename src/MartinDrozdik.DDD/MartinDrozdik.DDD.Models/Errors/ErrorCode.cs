61732500ae45369dec725a8b2926dcf5fe444809
Martin Drozdík <drozdik.m@seznam.cz>
2025-03-25 18:08:10 +0100

    Add error code

---
+++ src/MartinDrozdik.DDD/MartinDrozdik.DDD.Models/Errors/ErrorCode.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace MartinDrozdik.DDD.Models.Errors;
+
+/// <summary>
+/// An error code that represents a specific identifier of an error type in the system.
+/// </summary>
+[DebuggerDisplay("{Key}")]
+public sealed class ErrorCode : ValueObject
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ErrorCode"/> class.
+    /// Validates that the key is not null or whitespace.
+    /// </summary>
+    /// <param name="key">The error code as string.</param>
+    public ErrorCode(string key)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(key);
+        Key = key;
+    }
+
+    /// <summary>
+    /// Gets code of the error. Each error should have a unique code.
+    /// </summary>
+    public string Key { get; }
+
+    /// <summary>
+    /// An implicit casting from string to <see cref="ErrorCode"/>.
+    /// </summary>
+    /// <param name="code">The error code as string.</param>
+    public static implicit operator ErrorCode(string code) => new (code);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Key;
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Key;
+    }
+}
