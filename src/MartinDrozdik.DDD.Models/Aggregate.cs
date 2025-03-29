d6fd1f54431395c0e094ebfbb8964fe1de9e1024
Martin Drozdík <drozdik.m@seznam.cz>
2025-03-29 20:40:20 +0100

    Add aggregate

---
+++ src/MartinDrozdik.DDD.Models/Aggregate.cs
@@ -0,0 +1,18 @@
+﻿namespace MartinDrozdik.DDD.Models;
+
+/// <summary>
+/// An aggregate that represents a single usecase unit in a domain-driven design context.
+/// </summary>
+/// <typeparam name="TIdentity">The type of the aggregates' identity.</typeparam>
+public abstract class Aggregate<TIdentity> : Entity<TIdentity>
+    where TIdentity : notnull
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Aggregate{TIdentity}"/> class.
+    /// </summary>
+    /// <param name="id">The aggregate ID.</param>
+    protected Aggregate(TIdentity id)
+        : base(id)
+    {
+    }
+}
