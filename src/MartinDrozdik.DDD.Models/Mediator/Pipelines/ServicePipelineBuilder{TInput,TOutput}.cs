85162c4a3e122d58d59f7ba8457ad4578c6fc211
Martin Drozdík <drozdik.m@seznam.cz>
2025-08-14 18:39:11 +0200

    -

---
+++ src/MartinDrozdik.DDD.Models/Mediator/Pipelines/ServicePipelineBuilder{TInput,TOutput}.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MartinDrozdik.DDD.Models.Mediator.Pipelines;
+
+/// <summary>
+/// Builds a pipeline of behaviors that can process input and produce output.
+/// Build is done via <see cref="IServiceProvider"/> to enable dependency injection for the behaviors.
+/// </summary>
+/// <typeparam name="TInput">Type of the input.</typeparam>
+/// <typeparam name="TOutput">Type of the output.</typeparam>
+public class ServicePipelineBuilder<TInput, TOutput>
+{
+    private readonly List<Type> _pipelineTypes = [];
+
+    /// <summary>
+    /// Adds a behaviour step to the pipeline.
+    /// </summary>
+    /// <typeparam name="TPipelineBehaviour">Type of the pipeline to build and invoke.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServicePipelineBuilder<TInput, TOutput> Add<TPipelineBehaviour>()
+        where TPipelineBehaviour : IPipelineBehavior<TInput, TOutput>
+    {
+        return Add(typeof(TPipelineBehaviour));
+    }
+
+    /// <summary>
+    /// Adds a behaviour step to the pipeline.
+    /// </summary>
+    /// <param name="pipelineType">Type of the pipeline to build and invoke.</param>
+    /// <returns>This for chaining.</returns>
+    public ServicePipelineBuilder<TInput, TOutput> Add(Type pipelineType)
+    {
+        // Validate the pipeline type
+        ArgumentNullException.ThrowIfNull(pipelineType);
+        if (!typeof(IPipelineBehavior<TInput, TOutput>).IsAssignableFrom(pipelineType))
+        {
+            throw new ArgumentException($"Type {pipelineType.Name} does not implement {nameof(IPipelineBehavior<TInput, TOutput>)}", nameof(pipelineType));
+        }
+
+        // Add the pipeline to the start of the list to ensure it is executed first
+        _pipelineTypes.Insert(0, pipelineType);
+        return this;
+    }
+
+    /// <summary>
+    /// Returns the entry point of the pipeline.
+    /// </summary>
+    /// <param name="serviceProvider">Service provider to resolve the pipeline behaviors.</param>
+    /// <returns>Pipeline behavior that represents the entry point of the pipeline.</returns>
+    public IPipelineBehavior<TInput, TOutput> Build(IServiceProvider serviceProvider)
+    {
+        if (_pipelineTypes.Count == 0)
+        {
+            return EmptyPipeline<TInput, TOutput>.Instance;
+        }
+
+        // Resolve the pipeline behaviors from the service provider
+        var reversedPipelines = _pipelineTypes
+            .Select(type => (IPipelineBehavior<TInput, TOutput>)serviceProvider.GetRequiredService(type))
+            .ToArray();
+
+        // Create the pipeline with the behaviors
+        return new Pipeline<TInput, TOutput>(reversedPipelines);
+    }
+}
---
+++ src/MartinDrozdik.DDD.Models/Mediator/ServiceMediatorConfig.cs
@@ -0,0 +1,185 @@
+﻿using MartinDrozdik.DDD.Models.Mediator.Commands;
+using MartinDrozdik.DDD.Models.Mediator.Pipelines;
+using MartinDrozdik.DDD.Models.Mediator.Queries;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MartinDrozdik.DDD.Models.Mediator;
+
+/// <summary>
+/// Configuration class for the Mediator service.
+/// </summary>
+/// <param name="services">Services to register handlers to.</param>
+public class ServiceMediatorConfig(IServiceCollection services)
+{
+    /// <inheritdoc cref="WithQuery{TQuery, TResponse, THandler}(ServicePipelineBuilder{TQuery, TResponse})"/>
+    public ServiceMediatorConfig WithQuery<TQuery, TResponse, THandler>()
+        where TQuery : IQuery<TResponse>
+        where THandler : class, IQueryHandler<TQuery, TResponse>
+    {
+        services.AddScoped<IQueryHandler<TQuery, TResponse>, THandler>();
+        return this;
+    }
+
+    /// <summary>
+    /// Registers a query handler for a specific query type and response type.
+    /// </summary>
+    /// <param name="pipelineBuilder">Pipeline builder with added pipeline types.</param>
+    /// <typeparam name="TQuery">The type of the query.</typeparam>
+    /// <typeparam name="TResponse">The type of the query response.</typeparam>
+    /// <typeparam name="THandler">The type of the query handler.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServiceMediatorConfig WithQuery<TQuery, TResponse, THandler>(ServicePipelineBuilder<TQuery, TResponse> pipelineBuilder)
+        where TQuery : IQuery<TResponse>
+        where THandler : class, IQueryHandler<TQuery, TResponse>
+    {
+        WithQuery<TQuery, TResponse, THandler>();
+        services.AddScoped(pipelineBuilder.Build);
+        return this;
+    }
+
+    /// <inheritdoc cref="WithCommand{TCommand, TResponse, THandler}(ServicePipelineBuilder{TCommand, TResponse})"/>/>
+    public ServiceMediatorConfig WithCommand<TCommand, TResponse, THandler>()
+        where TCommand : ICommand<TResponse>
+        where THandler : class, ICommandHandler<TCommand, TResponse>
+    {
+        services.AddScoped<ICommandHandler<TCommand, TResponse>, THandler>();
+        return this;
+    }
+
+    /// <summary>
+    /// Registers a command handler for a specific command type and response type.
+    /// </summary>
+    /// <param name="pipelineBuilder">Pipeline builder with added pipeline types.</param>
+    /// <typeparam name="TCommand">The type of the command.</typeparam>
+    /// <typeparam name="TResponse">The type of the command response.</typeparam>
+    /// <typeparam name="THandler">The type of the command handler.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServiceMediatorConfig WithCommand<TCommand, TResponse, THandler>(ServicePipelineBuilder<TCommand, TResponse> pipelineBuilder)
+        where TCommand : ICommand<TResponse>
+        where THandler : class, ICommandHandler<TCommand, TResponse>
+    {
+        WithCommand<TCommand, TResponse, THandler>();
+        services.AddScoped(pipelineBuilder.Build);
+        return this;
+    }
+
+    /// <inheritdoc cref="WithCommand{TCommand, THandler}(ServicePipelineBuilder{TCommand})"/>/>
+    public ServiceMediatorConfig WithCommand<TCommand, THandler>()
+        where TCommand : ICommand
+        where THandler : class, ICommandHandler<TCommand>
+    {
+        WithCommand<TCommand, THandler>();
+        return this;
+    }
+
+    /// <summary>
+    /// Registers a command handler for a specific command type and response type.
+    /// </summary>
+    /// <param name="pipelineBuilder">Pipeline builder with added pipeline types.</param>
+    /// <typeparam name="TCommand">The type of the command.</typeparam>
+    /// <typeparam name="THandler">The type of the command handler.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServiceMediatorConfig WithCommand<TCommand, THandler>(ServicePipelineBuilder<TCommand> pipelineBuilder)
+        where TCommand : ICommand
+        where THandler : class, ICommandHandler<TCommand>
+    {
+        services.AddScoped<ICommandHandler<TCommand>, THandler>();
+        services.AddScoped(pipelineBuilder.Build);
+        return this;
+    }
+
+    /// <summary>
+    /// Searches for all request handlers in the specified assembly and registers them.
+    /// </summary>
+    /// <typeparam name="TAssembly">Type of the assembly to go throught.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServiceMediatorConfig WithRequestsFromAssembly<TAssembly>()
+    {
+        // Get all non-abstract types in the assembly
+        var nonAbstractTypes = GetAssemblyNonAbstractTypes<TAssembly>();
+
+        // Register all handlers
+        RegisterHandlerTypes(nonAbstractTypes, typeof(IQueryHandler<,>));
+        RegisterHandlerTypes(nonAbstractTypes, typeof(ICommandHandler<,>));
+        RegisterHandlerTypes(nonAbstractTypes, typeof(ICommandHandler<>));
+
+        return this;
+
+        void RegisterHandlerTypes(IEnumerable<Type> nonAbstractTypes, Type handlerType)
+        {
+            foreach (var item in GetTypeWithSearchedInterface(nonAbstractTypes, handlerType))
+            {
+                services.AddScoped(item.Interface, item.ActualType);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Searches for all pipeline behaviours in the specified assembly and registers them for all commands and queries.
+    /// </summary>
+    /// <typeparam name="TAssembly">Type of the assembly to go throught.</typeparam>
+    /// <returns>This for chaining.</returns>
+    public ServiceMediatorConfig WithPipelinesFromAssembly<TAssembly>()
+    {
+        // Get all non-abstract types in the assembly
+        var nonAbstractTypes = GetAssemblyNonAbstractTypes<TAssembly>();
+
+        // Register all handlers
+        RegistePipelineTypes(nonAbstractTypes, typeof(IQueryHandler<,>));
+        RegistePipelineTypes(nonAbstractTypes, typeof(ICommandHandler<,>));
+        RegistePipelineTypes(nonAbstractTypes, typeof(ICommandHandler<>));
+
+        return this;
+
+        void RegistePipelineTypes(IEnumerable<Type> nonAbstractTypes, Type handlerType)
+        {
+            foreach (var item in GetTypeWithSearchedInterface(nonAbstractTypes, handlerType))
+            {
+                var pipelineBuilderType = typeof(ServicePipelineBuilder<,>).MakeGenericType(item.Interface.GetGenericArguments());
+                // TODO finish
+                //services.AddScoped(item.Interface, item.ActualType);
+            }
+
+            /*var pipelineBuilderType = typeof(ServicePipelineBuilder<,>).MakeGenericType(item.Interface.GetGenericArguments());
+                services.AddScoped(pipelineBuilderType, sp => Activator.CreateInstance(pipelineBuilderType));
+                services.AddScoped(item.Interface, item.Handler);*/
+        }
+    }
+
+    /// <summary>
+    /// Returns all non-abstract types from the specified assembly.
+    /// </summary>
+    /// <typeparam name="TAssembly">Type of the assembly</typeparam>
+    /// <returns>All non abstract types from the assembly.</returns>
+    private static Type[] GetAssemblyNonAbstractTypes<TAssembly>()
+    {
+        // Get the assembly of the specified type
+        var assembly = typeof(TAssembly).Assembly;
+
+        // Get all non-abstract types in the assembly
+        var nonAbstractTypes = assembly
+            .GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract)
+            .ToArray();
+
+        return nonAbstractTypes;
+    }
+
+    /// <summary>
+    /// Returns all types that implement the searched interface from the specified non-abstract type list.
+    /// </summary>
+    /// <param name="typeList">List of types to check.</param>
+    /// <param name="searchedInterface">The interface to look for.</param>
+    private static ClassWithInterface[] GetTypeWithSearchedInterface(IEnumerable<Type> typeList, Type searchedInterface)
+    {
+        var numberOfGenericArguments = searchedInterface.GetGenericArguments().Length;
+        var typesWithInterfaces = typeList
+                    .SelectMany(t => t.GetInterfaces()
+                        .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == searchedInterface && i.GetGenericArguments().Length == numberOfGenericArguments)
+                        .Select(i => new ClassWithInterface(t, i)))
+                    .ToArray();
+        return typesWithInterfaces;
+    }
+
+    private record struct ClassWithInterface(Type ActualType, Type Interface);
+}
