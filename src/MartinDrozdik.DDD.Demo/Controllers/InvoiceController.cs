c0e06cc8302e6b13a3a8f0896a97001aea2b9818
Martin Drozdík <drozdik.m@seznam.cz>
2025-11-21 12:26:48 +0100

    Add scalar profile

--- src/MartinDrozdik.DDD.Demo/Controllers/InvoiceController.cs
+++ src/MartinDrozdik.DDD.Demo/Controllers/InvoiceController.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace MartinDrozdik.DDD.Demo.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("v1/invoice")]
 public class InvoiceController : ControllerBase
 {
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetInvoice")]
     public ActionResult Get()
     {
         return Ok();
