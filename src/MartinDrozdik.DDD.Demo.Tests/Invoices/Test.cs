695e06659150c34199ff309ab3dd6a0c47efa1e8
Martin Drozdík <drozdik.m@seznam.cz>
2025-11-21 17:51:11 +0100

    Add sample test

---
+++ src/MartinDrozdik.DDD.Demo.Tests/Invoices/Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MartinDrozdik.DDD.Demo.Tests.Invoices;
+
+public class Test(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task SampleTest()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/v1/invoice");
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        //Assert.Equal("text/html; charset=utf-8",
+        //    response.Content.Headers.ContentType.ToString());
+    }
+}
