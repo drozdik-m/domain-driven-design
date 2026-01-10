11c0d73ca3853b062ed598a6699de39548f9d34f
Martin Drozdík <drozdik.m@seznam.cz>
2026-01-10 17:07:12 +0100

    Add random string extension

---
+++ src/MartinDrozdik.DDD.Models.Tests/Extensions/RandomExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using MartinDrozdik.DDD.Models.Extensions;
+
+namespace MartinDrozdik.DDD.Models.Tests.Extensions;
+
+public class RandomExtensionsTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(100)]
+    [InlineData(256)]
+    [InlineData(500)]
+    [InlineData(1000)]
+    [InlineData(2000)]
+    [InlineData(4000)]
+    [InlineData(8000)]
+    public void Random_string_returns_correct_length_with(int length)
+    {
+        var random = new Random(67);
+        var result = random.String(length, "abc");
+
+        Assert.Equal(length, result.Length);
+    }
+
+    [Theory]
+    [InlineData(RandomExtensions.Sets.Letters)]
+    [InlineData(RandomExtensions.Sets.Digits)]
+    [InlineData(RandomExtensions.Sets.Alphanumeric)]
+    [InlineData(RandomExtensions.Sets.Hex)]
+    [InlineData(RandomExtensions.Sets.Lowercase)]
+    [InlineData(RandomExtensions.Sets.Uppercase)]
+    [InlineData(RandomExtensions.Sets.UrlSafe)]
+    [InlineData(RandomExtensions.Sets.SpecialChars)]
+    [InlineData(RandomExtensions.Sets.AlphanumericSpecial)]
+    public void Random_string_returns_only_allowed_characters(string characters)
+    {
+        var random = new Random(69);
+        var resultShort = random.String(64, characters);
+        var resultLong = random.String(2000, characters);
+
+        Assert.Multiple(
+            () => Assert.All(resultShort, e => Assert.Contains(e, characters)),
+            () => Assert.All(resultLong, e => Assert.Contains(e, characters)));
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-10)]
+    [InlineData(-100)]
+    [InlineData(int.MinValue)]
+    public void Random_string_throws_on_negative_length(int length)
+    {
+        var random = new Random(71);
+        Assert.Throws<ArgumentOutOfRangeException>(() => random.String(length, "abc"));
+    }
+
+    [Fact]
+    public void Empty_allowed_characters_throws()
+    {
+        var random = new Random(73);
+        Assert.Throws<ArgumentException>(() => random.String(10, string.Empty));
+    }
+}
---
+++ src/MartinDrozdik.DDD.Models/Extensions/RandomExtensions.cs
@@ -0,0 +1,98 @@
+﻿namespace MartinDrozdik.DDD.Models.Extensions;
+
+/// <summary>
+/// Provides extension methods for the <see cref="Random"/> class.
+/// </summary>
+public static class RandomExtensions
+{
+    /// <summary>
+    /// Generates a random string of specified length using preset characters.
+    /// </summary>
+    /// <param name="random">The <see cref="Random"/> instance to use for generation.</param>
+    /// <param name="length">The length of the string to generate.</param>
+    /// <param name="allowedChars">The set of characters to choose from.</param>
+    /// <returns>A random string of the specified length.</returns>
+    public static string String(this Random random, int length, ReadOnlySpan<char> allowedChars)
+    {
+        ArgumentNullException.ThrowIfNull(random);
+
+        if (length < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be negative.");
+        }
+
+        if (allowedChars.IsEmpty)
+        {
+            throw new ArgumentException("Allowed characters cannot be empty.", nameof(allowedChars));
+        }
+
+        if (length == 0)
+        {
+            return string.Empty;
+        }
+
+        // Use stack allocation for small strings
+        const int stackAllocThreshold = 128;
+        if (length <= stackAllocThreshold)
+        {
+            Span<char> buffer = stackalloc char[stackAllocThreshold];
+
+            for (var i = 0; i < length; i++)
+            {
+                buffer[i] = allowedChars[random.Next(allowedChars.Length)];
+            }
+
+            return new string(buffer[..length]);
+        }
+
+        // For large strings, use string.Create
+        return string.Create(length, (random, allowedChars.ToString()), (span, state) =>
+        {
+            var chars = state.Item2.AsSpan();
+            for (var i = 0; i < span.Length; i++)
+            {
+                span[i] = chars[state.random.Next(chars.Length)];
+            }
+        });
+    }
+
+    /// <inheritdoc cref="String(Random, int, ReadOnlySpan{char})" />
+    public static string String(this Random random, int length, string allowedChars)
+    {
+        ArgumentNullException.ThrowIfNull(allowedChars);
+        return random.String(length, allowedChars.AsSpan());
+    }
+
+    /// <summary>
+    /// Common character sets for random string generation.
+    /// </summary>
+    public static class Sets
+    {
+        /// <summary>Lowercase letters (a-z).</summary>
+        public const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+        /// <summary>Uppercase letters (A-Z).</summary>
+        public const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        /// <summary>Digits (0-9).</summary>
+        public const string Digits = "0123456789";
+
+        /// <summary>Alphanumeric characters (a-z, A-Z, 0-9).</summary>
+        public const string Alphanumeric = Lowercase + Uppercase + Digits;
+
+        /// <summary>Letters only (a-z, A-Z).</summary>
+        public const string Letters = Lowercase + Uppercase;
+
+        /// <summary>Hexadecimal characters (0-9, a-f).</summary>
+        public const string Hex = "0123456789abcdef";
+
+        /// <summary>URL-safe characters (a-z, A-Z, 0-9, -, _).</summary>
+        public const string UrlSafe = Alphanumeric + "-_";
+
+        /// <summary>Common special characters.</summary>
+        public const string SpecialChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+        /// <summary>Alphanumeric with special characters.</summary>
+        public const string AlphanumericSpecial = Alphanumeric + SpecialChars;
+    }
+}
