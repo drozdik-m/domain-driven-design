777f71e1bc3326d7794e1ceeb30f0e5fece3768a
Martin Drozdík <drozdik.m@seznam.cz>
2026-01-12 19:13:22 +0100

    Add ToFriendlyFileName

---
+++ src/MartinDrozdik.DDD.Models.Tests/Extensions/PathExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using MartinDrozdik.DDD.Models.Extensions;
+
+namespace MartinDrozdik.DDD.Models.Tests.Extensions;
+
+public class PathExtensionsTests
+{
+    [Theory]
+    [InlineData("hello world", "hello-world")]
+    [InlineData("test  file", "test--file")]
+    [InlineData("my file.txt", "my-file.txt")]
+    [InlineData("file", "file")]
+    [InlineData("", "")]
+    [InlineData("test\tfile", "test-file")]
+    [InlineData("test\nfile", "test-file")]
+    [InlineData("test\r\nfile", "test--file")]
+    [InlineData("file<name>", "file-name-")]
+    [InlineData("file:name", "file-name")]
+    [InlineData("file|name", "file-name")]
+    [InlineData("file?name", "file-name")]
+    [InlineData("file*name", "file-name")]
+    [InlineData("file\"name", "file-name")]
+    [InlineData("file/name", "file-name")]
+    [InlineData("file\\name", "file-name")]
+    [InlineData("|filename", "-filename")]
+    [InlineData("filename|", "filename-")]
+    [InlineData("valid-filename.txt", "valid-filename.txt")]
+    [InlineData("my_file_123.doc", "my_file_123.doc")]
+    [InlineData("test.file.name", "test.file.name")]
+    [InlineData("file name with multiple   spaces", "file-name-with-multiple---spaces")]
+    [InlineData("   leading spaces", "---leading-spaces")]
+    [InlineData("trailing spaces   ", "trailing-spaces---")]
+    public void ToFriendlyFileName_returns_expected_result(string input, string expected)
+    {
+        Assert.Equal(expected, input.ToFriendlyFileName());
+    }
+}
---
+++ src/MartinDrozdik.DDD.Models/Extensions/PathExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace MartinDrozdik.DDD.Models.Extensions;
+
+/// <summary>
+/// Provides extension methods for path manipulation.
+/// </summary>
+public static class PathExtensions
+{
+    /// <summary>
+    /// Converts a string to a path-friendly format by replacing whitespace with hyphens
+    /// and removing invalid filename characters.
+    /// </summary>
+    /// <param name="name">The string to convert.</param>
+    /// <returns>A path-friendly name.</returns>
+    public static string ToFriendlyFileName(this string name)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+
+        var invalidChars = Path.GetInvalidFileNameChars();
+        return string.Create(name.Length, (name, invalidChars), (span, state) =>
+        {
+            state.name.AsSpan().CopyTo(span);
+
+            for (var i = 0; i < span.Length; i++)
+            {
+                var c = span[i];
+                if (char.IsWhiteSpace(c) || Array.IndexOf(state.invalidChars, c) >= 0)
+                {
+                    span[i] = '-';
+                }
+            }
+        });
+    }
+}
