296ad7ee5339928e105ace18f1b195a1901e2645
Martin Drozdík <drozdik.m@seznam.cz>
2026-01-29 20:32:27 +0100

    DemoAppFactory - sqlit to dir

--- src/MartinDrozdik.DDD.Demo.Tests/DemoAppFactory.cs
+++ src/MartinDrozdik.DDD.Demo.Tests/DemoAppFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Kiota.Http.HttpClientLibrary;
@@ -11,14 +12,26 @@ namespace MartinDrozdik.DDD.Demo.Tests;
 
 public class DemoAppFactory : WebApplicationFactory<Program>
 {
-    private readonly string _dbPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString(), "test_db.db");
+    private readonly string _dbDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+    private readonly string _dbPath;
     private readonly ITestOutputHelper _testOutputHelper;
     private readonly Action<IWebHostBuilder>? _config;
 
     public DemoAppFactory(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
+
+        _dbPath = Path.Combine(_dbDir, "test_demo_app.db");
         testOutputHelper.WriteLine($"Test database path: {_dbPath}");
+
+        if (!Directory.Exists(_dbDir))
+        {
+            Directory.CreateDirectory(_dbDir);
+        }
+        else
+        {
+            testOutputHelper.WriteLine($"!!! Test database directory already exists: {_dbDir}");
+        }
     }
 
     public DemoAppFactory(ITestOutputHelper testOutputHelper, Action<IWebHostBuilder> config)
@@ -70,14 +83,16 @@ public class DemoAppFactory : WebApplicationFactory<Program>
             // Clean up the test database file
             try
             {
-                if (File.Exists(_dbPath))
+                if (Directory.Exists(_dbDir))
                 {
-                    File.Delete(_dbPath);
+                    Directory.Delete(_dbDir, recursive: true);
                 }
             }
-            catch
+            catch (Exception e)
             {
-                // Ignore errors during cleanup
+                var logger = Services.GetRequiredService<ILogger<DemoAppFactory>>();
+                logger.LogError(e, "Failed to delete test database directory at {DbDir}", _dbDir);
+                throw;
             }
         }
 
